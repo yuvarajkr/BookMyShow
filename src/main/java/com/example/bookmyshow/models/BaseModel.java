8744ec79356211ac47553c4d68fdc196048aa503
yuvarajkr <yuvarajkr73@gmail.com>
2024-06-15 23:25:24 +0530

    some changes for last updated date

--- src/main/java/com/example/bookmyshow/models/BaseModel.java
+++ src/main/java/com/example/bookmyshow/models/BaseModel.java
@@ -5,19 +5,24 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id // PK
     @GeneratedValue(strategy=GenerationType.IDENTITY) //auto increment
     private int id;
+
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime lastModifiedAt;
